bb631dd0e5e549826f2027daf332182acdca94c4
alancyau <alancyau@gmail.com>
2022-11-28 15:46:03 -0600

    add graphs, boost, afr

--- readlog.py
+++ readlog.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 from collections import Counter
@@ -9,15 +10,16 @@ from typing import Union
 # Initial log filters
 VA_CLOSED_LOOP = 'on'
 VB_CLOSED_LOOP = 1
-ACC_POS = 25  # In percent
+ACC_POS = 100  # In percent
 
 # Monitor condition variables
 FK_TRESHOLD = -2  # In degrees. The more negative, the more timing retarded.
 FKL_THRESHOLD = -2  # In degrees. The more negative, the more timing retarded.
 DAM_TRESHOLD = 1
 OIL_TEMP_TRESHOLD = 250  # In Fahrenheit
-AF_CORRECT_POSITIVE_TRESHOLD = 18  # In percent
-AF_CORRECT_NEGATIVE_TRESHOLD = -18  # In percent
+AF_CORRECT_TRESHOLD = 18  # Amount of fuel added of subracted in percent.
+AF_THRESHOLD = 0.8
+BOOST_THRESHOLD = 1  # In PSI
 
 
 def load_csv(file: str) -> pd.DataFrame:
@@ -26,7 +28,7 @@ def load_csv(file: str) -> pd.DataFrame:
 
 
 def apply_global_filters(df: pd.DataFrame, monitors) -> pd.DataFrame:
-    df_acc = df[df[monitors['acc_pos']] > ACC_POS]
+    df_acc = df[df[monitors['acc_pos']] >= ACC_POS]
     df_cl = df_acc[df_acc[monitors['closed_loop']] == VA_CLOSED_LOOP]
     if len(df_cl):
         return df_cl
@@ -38,10 +40,13 @@ def apply_global_filters(df: pd.DataFrame, monitors) -> pd.DataFrame:
 def verify_log_monitors(df: pd.DataFrame) -> dict:
     df_columns = df.columns.to_list()
     monitors = {
+        'time': ('Time (sec)'),
         'af_ratio': ('AF Sens 1 Ratio (AFR)'),
         'af_comm': ('Comm Fuel Final (AFR)'),
         'af_corr': ('AF Correction 1 (%)'),
         'af_learn': ('AF Learning 1 (%)'),
+        'boost_actual':('Boost (psi)'),
+        'boost_target':('Target Boost Final Rel SL (psi)', 'Target Boost Final Rel. SL (psi)'),
         'oil_temp': ('Oil Temp (F)'),
         'acc_pos': ('Accel. Position (%)', 'Accel Position (%)'),
         'fk': ('Feedback Knock (°)', 'Feedback Knock (degrees)'),
@@ -52,7 +57,8 @@ def verify_log_monitors(df: pd.DataFrame) -> dict:
         'cyl3_rough': ('Roughness Cyl 3 (count)', 'Roughness Cyl 3 (misfire count)'),
         'cyl4_rough': ('Roughness Cyl 4 (count)', 'Roughness Cyl 4 (misfire count)'),
         'closed_loop': ('Closed Loop Sw. (on/off)', 'Closed Loop Sw (status)'),
-}
+        'rpm': ('RPM (RPM)')
+}       
     for k in monitors:
         for item in monitors[k]:
             if item in df_columns:
@@ -65,17 +71,39 @@ class MonitorConditions:
         self.df = df
         self.mon = monitors
         self.status_cyl_misfire = self.cylinder_misfire()
-        self.status_af_exceed = self.af_correct_exceed()
+        self.status_af_correct_exceed = self.af_correct_exceed()
         self.status_fk = self.feedback_knock()
         self.status_fkl = self.fineknock_learn()
         self.status_dam = self.dam_exceed()
         self.status_oil_temp = self.oil_temp_exceed()
+        self.status_af = self.af_command_vs_actual()
+        self.status_boost = self.boost_command_vs_actual()
 
     def af_command_vs_actual(self):
-        pass
+        self.df['AF Difference'] = abs(self.df[self.mon['af_ratio']] - self.df[self.mon['af_comm']])
+        af_diff = self.df['AF Difference'].to_list()
+        
+        if any(item >= AF_THRESHOLD for item in af_diff):
+            ax = plt.gca()
+            self.df.plot(kind='line',x=self.mon['time'],y=self.mon['af_ratio'],ax=ax)
+            self.df.plot(kind='line',x=self.mon['time'],y=self.mon['af_comm'], color='red', ax=ax)
+            plt.show()
+            return True
+        else:
+            return False
 
     def boost_command_vs_actual(self):
-        pass
+        self.df['Boost Difference'] = abs(self.df[self.mon['boost_target']] - self.df[self.mon['boost_actual']])
+        boost_diff = self.df['Boost Difference'].to_list()
+        
+        if any(item >= BOOST_THRESHOLD for item in boost_diff):
+            ax = plt.gca()
+            self.df.plot(kind='line',x=self.mon['time'],y=self.mon['boost_actual'],ax=ax)
+            self.df.plot(kind='line',x=self.mon['time'],y=self.mon['boost_target'], color='red', ax=ax)
+            plt.show()
+            return True
+        else:
+            return False
 
     def cylinder_misfire(self) -> tuple:
         cyl1 = self.df[self.mon['cyl1_rough']].to_list()
@@ -89,14 +117,9 @@ class MonitorConditions:
         return mf1, mf2, mf3, mf4
 
     def af_correct_exceed(self) -> bool:
-        self.df['AF Actual Correction (%)'] = self.df[self.mon['af_corr']] + self.df[self.mon['af_learn']]
+        self.df['AF Actual Correction (%)'] = abs(self.df[self.mon['af_corr']] + self.df[self.mon['af_learn']])
         af_actual = self.df['AF Actual Correction (%)'].to_list()
-        if any(item > AF_CORRECT_POSITIVE_TRESHOLD for item in af_actual):
-            return True
-        elif any(item < AF_CORRECT_NEGATIVE_TRESHOLD for item in af_actual):
-            return True
-        else:
-            return False
+        return True if any(item > AF_CORRECT_TRESHOLD for item in af_actual) else False
 
     def feedback_knock(self) -> Union[bool,Counter]:
         fk = self.df[self.mon['fk']].to_list()
@@ -126,10 +149,11 @@ def main() -> None:
         if status: print('Misfire detected')
     if conditions.status_fk: print(f'Feedback knock detected\n{conditions.status_fk}')
     if conditions.status_fkl: print(f'Fineknock learn detechted\n{conditions.status_fkl}')
-    if conditions.status_af_exceed: print('AF correction exceeded')
+    if conditions.status_af_correct_exceed: print('AF correction exceeded')
     if conditions.status_dam: print(f'Ending drive DAM dropped below {DAM_TRESHOLD}')
     if conditions.status_oil_temp: print(f'Oil temp exceeded {OIL_TEMP_TRESHOLD}')
-
+    if conditions.status_af: print(f'AF actual vs command diffference exceeded {AF_THRESHOLD}')
     
+
 if __name__ == "__main__":
     main()
